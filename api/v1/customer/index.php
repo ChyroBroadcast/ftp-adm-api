200fdeb22f055d6be58f7a970bb4c1b466b105dd
Damien Degorre <ddegorre@chyro.tv>
2015-10-19 10:39:39 +0200

    Add checkConnectedt()
    See: commit: 7477b2ac4eac6853dcdcfadd831bb06796d6e404

--- api/v1/customer/index.php
+++ api/v1/customer/index.php
@@ -7,29 +7,29 @@
 
 	switch ($_SERVER['REQUEST_METHOD']) {
 		case 'GET':
-			if (isset($_SESSION['user'])) {
-				$customer = $db_driver->getCustomer($_SESSION['user']['customer']);
-				if ($customer === null)
-					httpResponse(204, null);
-				if ($customer)
-					httpResponse(200, $customer);
-				httpResponse(500, null);
-			} else
-				httpResponse(401, array('message' => 'Not logged in'));
+			checkConnected();
+
+			$customer = $db_driver->getCustomer($_SESSION['user']['customer']);
+			if ($customer === null)
+				httpResponse(204, null);
+			if ($customer)
+				httpResponse(200, $customer);
+			httpResponse(500, null);
+
 			break;
 
 		case 'POST':
-			if (isset($_SESSION['user'])) {
-				$fields = httpParseInput();
-				$fields['id'] = $_SESSION['user']['customer'];
-				$res = $db_driver->updateCustomer($fields);
-				if ($res === true)
-					httpResponse(200, array('message' => 'ok !'));
-				if ($res)
-					httpResponse(400, array('message' => $res));
-				httpResponse(500, null);
-			} else
-				httpResponse(401, array('message' => 'Not logged in'));
+			checkConnected();
+
+			$fields = httpParseInput();
+			$fields['id'] = $_SESSION['user']['customer'];
+			$res = $db_driver->updateCustomer($fields);
+			if ($res === true)
+				httpResponse(200, array('message' => 'Successfully updated'));
+			if ($res)
+				httpResponse(400, array('message' => $res));
+			httpResponse(500, null);
+
 			break;
 
 		case 'OPTIONS':
