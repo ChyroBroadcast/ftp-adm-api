aa2a646d54c62d04af0defde5d8d3a01711f56bd
Clercin Guillaume <gclercin@intellique.com>
2015-10-20 18:48:07 +0200

    Add new function: Get information of connected user
    If user is connected, get all information.
    Used by ftp-adm-frontend.

---
+++ api/v1/user/index.php
@@ -0,0 +1,27 @@
+<?php
+	require_once('../lib/env.php');
+
+	require_once('http.php');
+	require_once('session.php');
+	require_once('db.php');
+
+	switch ($_SERVER['REQUEST_METHOD']) {
+		case 'GET':
+			checkConnected();
+
+			httpResponse(200, array(
+				'user' => $_SESSION['user']
+			));
+
+			break;
+
+		case 'OPTIONS':
+			httpOptionsMethod(HTTP_GET);
+			break;
+
+		default:
+			httpUnsupportedMethod();
+			break;
+	}
+?>
+
